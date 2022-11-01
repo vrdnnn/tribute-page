29546e55f065dba5ba4b976208d060ec5d564856
vrdnnn <gabrilvrdn@gmail.com>
2022-11-01 15:27:12 -0600

    element deleted

---
+++ 8oct26/practicas/1-variables.js
@@ -0,0 +1,13 @@
+const myName = 'moni';
+let myAge = 19;
+let x = 5;
+let y = 'valor';
+var likesMusic = true;
+
+
+//typeof da el tipo de dato que sea(booleano, null, undefined, number, string)
+console.log(typeof myName);
+console.log(typeof myAge);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof likesMusic);
